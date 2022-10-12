c030e2e458825e5ca37bbfbd45f003ff91e29dd3
shakeeluddin <s_uddin91@hotmail.com>
2022-10-12 13:51:54 +0100

    update

--- route.js
+++ route.js
@@ -1,5 +1,7 @@
+
 const router = require ("express").router();
 const {movieSchema} = require ("../db");
+const movie = require("./Mongoose");
 
 router.get("getAllMovies",(req,res)=> movieSchema.find({}).then(results =>res.send(results)).catch(err => next (err)));
 
@@ -9,4 +11,33 @@ router.get("/getMovie/:id",(req,res,next) =>{
     res.send(movie[id])
 
 });
+
+router.post(`/newMovie`,async(req,res,next) => {
+    if (!req.body.name) return next ({status:400, message: "missing movie name"})
+    try {
+        const result = await movieSchema.create (req.body);
+        res.status(201).send(result);
+    } catch (err) {
+        return next (err)
+    }
+    
+});
+
+router.patch(`/replace/:id`, async (req,res,next) => {
+    try {
+        await movieSchema.findByNameAndYear(req.params.id,req.query)
+        const newMovie = await movieSchema.findByName(req.params.id);
+        res.send (newMovie);
+    } catch (err){
+        return (err);
+    }
+    });
+
+router.delete(`/delete/:id`,(req,res,next) => {
+    const {id} = req.params;
+    console.log("movie name",id);
+    movieSchema.findByNameAndYear(id).then(result => res.send(result)).catch(err => next (err));
+});
+
+module.exports = router;
 
